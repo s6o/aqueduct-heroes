afad2d4f1ca8d8043fc99dc8e5a88af07871e9da
Oliver Sõro <oliver.s6ro@gmail.com>
2021-01-29 22:35:12 +0200

    Initial commit of generated files: aqueduct create aqueduct_heros.

---
+++ lib/aquedact_heros.dart
@@ -0,0 +1,11 @@
+/// aquedact_heros
+///
+/// A Aqueduct web server.
+library aquedact_heros;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
