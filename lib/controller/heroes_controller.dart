e7c0bb232a7620217628fce4cef0fcd79f6c6f9e
Oliver Sõro <oliver.s6ro@gmail.com>
2021-01-30 17:48:40 +0200

    Update HeroesController to use database.

--- lib/controller/heroes_controller.dart
+++ lib/controller/heroes_controller.dart
@@ -1,32 +1,28 @@
+import 'package:aquedact_heros/model/hero.dart';
 import 'package:aqueduct/aqueduct.dart';
 
 class HeroesController extends ResourceController {
-  final ManagedContext context;
-
   HeroesController(this.context);
 
-  final _heroes = [
-    {'id': 11, 'name': 'Mr. Nice'},
-    {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name': 'Bombasto'},
-    {'id': 14, 'name': 'Celeritas'},
-    {'id': 15, 'name': 'Magneta'},
-  ];
+  final ManagedContext context;
 
   @Operation.get()
   Future<Response> getAllHeroes() async {
-    return Response.ok(_heroes);
+    final heroQuery = Query<Hero>(context);
+    final heroes = await heroQuery.fetch();
+
+    return Response.ok(heroes);
   }
 
   @Operation.get('id')
   Future<Response> getHeroByID(@Bind.path('id') int id) async {
-    final hero =
-        _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+    final heroQuery = Query<Hero>(context)..where((h) => h.id).equalTo(id);
+
+    final hero = await heroQuery.fetchOne();
 
     if (hero == null) {
       return Response.notFound();
     }
-
     return Response.ok(hero);
   }
 }
